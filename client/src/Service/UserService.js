4e687cfa8e4f8ec5d8af05f7bdb08293142a3744
Praneeth S liyanage <praneethsbliyanage@gmail.com>
2025-08-29 22:04:00 +0530

    Implemented new feature

--- client/src/Service/UserService.js
+++ client/src/Service/UserService.js
@@ -1,2 +1,7 @@
 import axios from "axios";
 
+export const addUser = async (user)  => {
+    return await axios.post('http://localhost:8080/api/v1.0/admin/register', user, {headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}});
+}
+
+export const 
